919fe17d088f99196b6f240472c72a58430b1492
Skarmony147 <skarmony147@gmail.com>
2026-04-20 08:54:23 -0500

    Update EManage.java

--- EManage.java
+++ EManage.java
@@ -1,1 +1,20 @@
 //This class will hold all of the employee items and functions
+import java.util.ArrayList;
+
+
+public class EManage{
+	
+	private ArrayList<String> workers = new ArrayList<>();
+	
+	public EMangage(){
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
