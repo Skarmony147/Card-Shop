e1be2b9c15ac1a41a65521c89d762f549eaa5e01
OtherTimeLoading <milnecharlie@pleasval.org>
2026-04-21 08:45:48 -0500

    Login window without functionality so far

---
+++ FinalProject/MainProgram.java
@@ -0,0 +1,13 @@
+import javax.swing.*; // Swing graphics
+import java.awt.*; // Misc things as needed
+import java.awt.event.*; // Event listeners and such
+
+/**
+ * Main program for final project by Sebastian and Charlie
+ * */
+
+public class MainProgram {
+	public static void main(String[] args){
+		
+	}
+}
