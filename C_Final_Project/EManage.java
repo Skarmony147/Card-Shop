b906ced974567c3cafa4bb706ad41f823b151568
Skarmony147 <skarmony147@gmail.com>
2026-04-21 08:27:04 -0500

    Add files via upload

---
+++ C_Final_Project/EManage.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+
+public class EManage{
+	
+	private ArrayList<String> workers = new ArrayList<>();
+	
+	public EMangage(){
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ C_Final_Project/SManage.java
@@ -0,0 +1,1 @@
+//This file will allow both managers and employees to access the stocks and to buy and sell based on clearance
