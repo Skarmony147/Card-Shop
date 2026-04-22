da5aca2b9bdecedab584b46b125d253bd9754467
Skarmony147 <skarmony147@gmail.com>
2026-04-22 08:43:45 -0500

    Update and rename SManage.java to Stocks.java

--- C_Final_Project/SManage.java
+++
@@ -1,9 +0,0 @@
-//This file will allow both managers and employees to access the stocks and to buy and sell based on clearance
-public class SManage {
-	public static void main(String[] args){
-		System.out.print("Hello");
-		
-		
-	}
-	
-}
