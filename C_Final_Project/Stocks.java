6625389f924d2e4fbba8465ce05908c25c6a0cb6
Skarmony147 <skarmony147@gmail.com>
2026-04-23 08:54:08 -0500

    Update Stocks.java

--- C_Final_Project/Stocks.java
+++ C_Final_Project/Stocks.java
@@ -6,7 +6,9 @@ public class Stocks {
 	private ArrayList<String> cards = new ArrayList<String>();	//This sets up the list which tells the current cards in stock
 	private ArrayList<Integer> cardInven = new ArrayList<Integer>();
 	
-	
+	/**
+	 * The constructor allows the user to have a basic inventory set up of card brands
+	 */
 	public Stocks(){	//This constructor sets up the inventory for the store to use
 		cards.add("Pokemon");
 		cards.add("Baseball");
@@ -19,19 +21,44 @@ public class Stocks {
 	}
 	
 	
-	
+	/**
+	 * This method allows the user to buy card brands for their card shop
+	 */
 	public void BuyStock(String cardType, int amount){
 		if(cards.get(0).equals(cardType))
 			cardInven.set(0, cardInven.get(0) + amount);
-		
-		
-		 
-		
+		else if(cards.get(1).equals(cardType))
+			cardInven.set(1, cardInven.get(1) + amount);
+		else if(cards.get(2).equals(cardType))
+			cardInven.set(2, cardInven.get(2) + amount);
+		else if(cards.get(3).equals(cardType))
+			cardInven.set(3, cardInven.get(3) + amount);	
+		else
+			System.out.println("There was an error with your selected card brand and the purchase did not go through. Please ensure spelling is correct.");
+	}
+	/**
+	 * This method allows the user to view current stock for a specific card brand
+	 * 
+	 * @param cardType takes in a string to find the current stock
+	 */	
+	public int ViewStock(String cardType){
+		if(cards.get(0).equals(cardType))
+			return(cardInven.get(0));
+		else if(cards.get(1).equals(cardType))
+			return(cardInven.get(1));
+		else if(cards.get(2).equals(cardType))
+			return(cardInven.get(2));
+		else if(cards.get(3).equals(cardType))
+			return(cardInven.get(3));	
+		else{
+			System.out.println("There was an error with your selected card brand and it could not be found. Please ensure spelling is correct.");
+			return(-1);
+		}
 	}
 	
 	
 	
 	public String toString(){
-		return("Currnt items in stock are:\n" + cards + "\nCurrent amounts are:\n" + cardInven);
+		return("Currnt items in stock are:\n" + cards + "\nCurrent amounts are:\n" + cardInven + "\n");
 	}
 }
