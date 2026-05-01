b7e17d052f4df6409748b391b994afec575e0cb8
Skarmony147 <skarmony147@gmail.com>
2026-05-01 08:50:17 -0500

    Update Management.java

--- C_Final_Project/Management.java
+++ C_Final_Project/Management.java
@@ -5,7 +5,7 @@ public class Management{
 	
 	private ArrayList<String> workers = new ArrayList<String>();	//This array is all your possible workers workin gat your store
 	private ArrayList<Double> hourlyWage = new ArrayList<Double>();	//This array is how much each of them gets paid
-	private ArrayList<String> currentWorkers = new ArrayList<String>();	//This array is which two of your workers are in rotation at the moment
+	private ArrayList<String> currentEmployees = new ArrayList<String>();	//This array is which two of your workers are in rotation at the moment
 	
 	
 	public Management(){
@@ -18,27 +18,29 @@ public class Management{
 			hourlyWage.add(14.50);
 		}
 		
-		Random rando = new Random(5);
+		Random rando = new Random();
 		boolean dupe = true;
 		while(dupe){
-			String wrkr1 = workers.get(rando.nextInt(4));	//make sure that each time is random
-			System.out.println(wrkr1);
+			String wrkr1 = workers.get(rando.nextInt(4));
 			String wrkr2 = workers.get(rando.nextInt(4));
-			System.out.println(wrkr2);
 			if(wrkr1.equals(wrkr2)){
 				continue;
 			}
 			else
 				dupe = false;
+		currentEmployees.add(wrkr1);
+		currentEmployees.add(wrkr2);
 		}
 	}
 	
-	
+	public String inHouse(){
+		return("Current employees working are: " + currentEmployees.get(0) + " and " + currentEmployees.get(1));
+	}
 	//For future reference, make a method called shiftChange to swap out our current employees. use the random number stuff.
 	
 	
 	public String toString(){
-		return("Your current workers are: " + workers + "\nHourly wages are: " + hourlyWage + "\n");  
+		return("Your currently employed employees are: " + workers + "\nHourly wages are: " + hourlyWage + "\n");  
 		
 	}
 	
