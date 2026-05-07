e026e3b02bcffc4465e2a71a161f2beb39438fd8
Skarmony147 <skarmony147@gmail.com>
2026-05-07 08:55:22 -0500

    Update Management.java

--- C_Final_Project/Management.java
+++ C_Final_Project/Management.java
@@ -7,7 +7,9 @@ public class Management{
 	private ArrayList<Double> hourlyWage = new ArrayList<Double>();	//This array is how much each of them gets paid
 	private ArrayList<String> currentEmployees = new ArrayList<String>();	//This array is which two of your workers are in rotation at the moment
 	
-	
+	/**
+	 * This constructor sets up a basic card shop with a set amount of workers and money.
+	 */
 	public Management(){
 		//Maybe make a list of a bunch of names for the constructor to choose from.
 		workers.add("Bob");	//Adding workers into the array
@@ -15,7 +17,7 @@ public class Management{
 		workers.add("Mikayla");
 		workers.add("Jim");
 		
-		for(int i = 0; i < 5; i++){	//Adding pay to array
+		for(int i = 0; i < 4; i++){	//Adding pay to array
 			hourlyWage.add(14.50);
 		}
 		
@@ -33,8 +35,17 @@ public class Management{
 		currentEmployees.add(wrkr2);
 		}
 	}
+	
+	/**
+	 * This is an overloaded constructor that allows you to customize your shop more than the basic one.
+	 * That includes custom worker names and custom pay
+	 * 
+	 * @param passer is a string ArrayList and is the list of names you would like to have as workers.
+	 * @param money is your custom amount of pay you are paying every memeber of the card shop.
+	 */
 	public Management(ArrayList<String> passer, double money){
 		workers = passer;
+		//(Optional)
 		//Make sure they can only have max of six workers using a while loop to check and see if it equals six and a scanner maybe?
 		
 		for(int i = 0; i < workers.size(); i++){	//Adding pay to array
@@ -57,7 +68,7 @@ public class Management{
 		
 	}
 	/**
-	 * This function tells you what employees are currently working.
+	 * This method tells you what employees are currently working.
 	 */
 	public String inHouse(){
 		return("Current employees working are: " + currentEmployees.get(0) + " and " + currentEmployees.get(1));
@@ -71,9 +82,13 @@ public class Management{
 	 */
 	public void Fire(String employee, String reason){
 		boolean notFound = true;
-		for(int i = 0; i < workers.size(); i++){
+		String statement = reason;
+		if(reason.length() < 6)
+			statement = "...";
+		
+		for(int i = 0; i < workers.size(); i++){	//Checks to see if the employee you want to fire is there.
 			if(workers.get(i).equals(employee)){
-				System.out.printf("Fired %s. When questioned, the manager said, '%s'\n", employee, reason);
+				System.out.printf("Fired %s. When questioned, the manager said, '%s'\n", employee, statement);
 				workers.remove(i);
 				hourlyWage.remove(i);
 				notFound = false;
@@ -81,12 +96,32 @@ public class Management{
 			}
 		
 		}
-		if(notFound){
+		if(notFound)
 			System.out.println("Selected employee was not found. Please double check spelling of their name.");
-		}
 		
 	}
 	
+	//Add hire function here
+	public void Hire(String name, double pay){
+		if(workers.size() < 6){
+			System.out.printf("A new member has join the ranks. Please welcome %s!\n", name);
+			workers.add(name);
+			hourlyWage.add(pay);
+		}
+		else
+			System.out.println("You have too many workers than this shop can contain/pay!");
+		
+		
+		
+	}
+	
+	
+	
+	//Add a method to change the pay of your workers
+	/**
+	 * shiftChange allows for the user to have the shift change and get new people in. 
+	 * This has no real value besides acting like your people get a break/chnaging out character names.
+	 */
 	public void shiftChange(){
 		String current1 = currentEmployees.get(0);
 		String current2 = currentEmployees.get(1);
@@ -110,7 +145,9 @@ public class Management{
 		}
 		
 	}
-	
+	/**
+	 * overrides the basic toString method and instead displays all of the workers you currently have hired and how much they are getting paid.
+	 */
 	public String toString(){
 		return("Your currently employed employees are: " + workers + "\nHourly wages are: " + hourlyWage + "\n");  
 		
