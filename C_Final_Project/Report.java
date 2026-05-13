b7be2a67bb3004088e4c1657f4c85694122dba64
Skarmony147 <skarmony147@gmail.com>
2026-05-13 08:53:44 -0500

    Update Report.java

--- C_Final_Project/Report.java
+++ C_Final_Project/Report.java
@@ -2,14 +2,19 @@ import java.io.*;
 
 public class Report{
 	
+	/*try{
+		PrintWriter printy = new PrintWriter("DailyReport.txt");
+		
+		
+	}
+	catch(Exception e){
+		System.out.println("There was an error with your daily report: " + e);
+	}
 	
 	
+	This is the idea for the Report thingy, but its bugging out.
 	
-	
-	
-	
-	
-	
+	*/
 	
 	
 	
